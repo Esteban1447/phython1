92785d2ad84b42f28c9ce75b8a71b9e6a0278e7e
Esteban1447 <esteban.robledo.rc@gmail.com>
2025-03-14 09:56:41 -0500

    creacion de primeras clases

--- piloto-proyecto.py
+++ piloto-proyecto.py
@@ -1,1 +1,15 @@
-# app
+profesor = ""
+estudiante = ""
+
+validacion = input("Estudiante o profesor: ")
+
+if validacion.lower() == "estudiante":
+    estudiante = input("Estudiante:"+"\n"
+                       +"Digite el nombre ")
+    profesor = "profesor 1"
+elif validacion.lower() == "profesor":
+        profesor = input("Profesor:"+"\n"
+                         +"Digite el nombre ")
+        estudiantes = "estudiante 1"
+
+print(estudiante+" "+profesor)
